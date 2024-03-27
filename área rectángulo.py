338d9549f8fcca90e862394c3aeb137c8a5eb406
elsinemer <164952107+elsinemer@users.noreply.github.com>
2024-03-26 22:12:13 -0300

    tema1-1des2
    Cálculo del área de un rectángulo con explicación.

---
+++ área rectángulo.py
@@ -0,0 +1,4 @@
+ancho = 10
+largo = 5
+area= ancho * largo
+print('Para calcular el área de un rectángulo debes multiplicar el ancho por el largo en este caso: ' + str(largo) + ' es el valor del largo y ' + str(ancho) + ' es el valor del ancho por lo tanto el resultado es: ' + str(area))
